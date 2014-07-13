149bb2969c28e7b238ad04ae774d3081b60aacc9
Brook Zhou <zz3599@yahoo.com>
2014-07-13 10:51:46 -0700

    Rip html from http://socket.io/get-started/chat/

--- src/server/server.js
+++ src/server/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
+var path = require('path');
 var sqlite = require('sqlite3').verbose();
 var app = express();
 
 var db = new sqlite.Database(':memory:');
+
+var newMessages = [];
+var requestQueue = [];
 
 db.serialize(function(){
     db.run("CREATE TABLE messages (message TEXT)");
@@ -30,8 +34,11 @@ app.route('/chat').get(function(req, res, next){
     console.log(req.param('message'));
     var m = req.param('message');
     var rows = [];
-    if(req.param('message')){
-        db.run("INSERT INTO messages VALUES(?)", m);
+    if(m){
+        db.run("INSERT INTO messages VALUES(?)", m, function(err){
+            console.log(this);
+            newMessages.push(m);
+        });
         getRows(logRow);
         console.log('inserted message');
         res.send(200);
@@ -40,17 +47,50 @@ app.route('/chat').get(function(req, res, next){
             result = '';
             for(var i = 0; i < rows.length; i++){
                 var row = rows[i];
-                result += '<h1>' + row.message + '</h1>' 
+                result += '<p>' + row.message + '</p>' 
             }
             res.send(result);
 
         });
 
-    }
-    
+    }    
 });
 
+app.route('/poll').get(function(req, res, next){
+    requestQueue.push({'request': req, 'timestamp' : new Date().getTime()});
+    if(newMessages.length !== 0){
+        var str = '';
+        for(var j = 0; j < newMessages.length; j++){
+            var message = newMessages[j];
+            str += '<p>' + message + '</p>';
+        }
+        console.log(str);
+        for(var i = 0; i < requestQueue.length; i++){
+            var requestItem = requestQueue[i];
+            requestItem.request.send(str);
+        }    
+        requestQueue = [];
+        newMessages = [];
 
+    }
+});
+
+app.get('/', function(req, res){
+  res.sendfile(path.resolve('../index.html'));
+});
+
+(function clearOldRequests(){
+    setTimeout(function(){
+        var minTime = new Date().getTime() - 30000;
+        for(var i = 0; i < requestQueue.length; i++){
+            if(requestQueue.timestamp < minTime){
+                requestQueue[i].send(200);
+                requestQueue.splice(i, 1);
+            }
+        }
+        clearOldRequests();
+    }, 3000);
+})();
 
 
 app.listen(3000);
